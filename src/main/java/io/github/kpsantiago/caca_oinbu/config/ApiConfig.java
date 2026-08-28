662e0a75f37645b287e63aaa243a8cfa9dd9bd8a
Kaua Santiago <santiago.kauajose12@gmail.com>
2026-08-28 08:39:43 -0300

    Refactor: add swagger paths to ApiConfig

--- src/main/java/io/github/kpsantiago/caca_oinbu/config/ApiConfig.java
+++ src/main/java/io/github/kpsantiago/caca_oinbu/config/ApiConfig.java
@@ -4,4 +4,11 @@ public class ApiConfig {
     public final static String API_BASE_PATH = "/api";
     public final static String AUTH_BASE_PATH = API_BASE_PATH + "/auth";
     public final static String USER_BASE_PATH = API_BASE_PATH + "/users";
+    public final static String[] SWAGGER_PATHS = {
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui.html/**",
+            "/swagger-ui/**"
+    };
 }
