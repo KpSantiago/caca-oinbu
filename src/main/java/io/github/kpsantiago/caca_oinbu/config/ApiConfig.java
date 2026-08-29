b92b2389536a824f760d0dedb5b21970c313710c
Kaua Santiago <santiago.kauajose12@gmail.com>
2026-08-29 11:32:37 -0300

    Fix: fix migration name and organize ApiConfig and whitelist

--- src/main/java/io/github/kpsantiago/caca_oinbu/config/ApiConfig.java
+++ src/main/java/io/github/kpsantiago/caca_oinbu/config/ApiConfig.java
@@ -2,13 +2,22 @@ package io.github.kpsantiago.caca_oinbu.config;
 
 public class ApiConfig {
     public final static String API_BASE_PATH = "/api";
-    public final static String AUTH_BASE_PATH = API_BASE_PATH + "/auth";
-    public final static String USER_BASE_PATH = API_BASE_PATH + "/users";
-    public final static String[] SWAGGER_PATHS = {
+    public final static String API_VERSION = "/v1";
+
+    public final static String API_BASE_VERSION = API_BASE_PATH + API_VERSION;
+
+    public final static String AUTH_BASE_PATH = API_BASE_VERSION + "/auth";
+    public final static String USER_BASE_PATH = API_BASE_VERSION + "/users";
+    public final static String BUS_BASE_PATH = API_BASE_VERSION + "/bus";
+
+    public final static String[] WHITE_LIST = {
             "/v3/api-docs",
             "/v3/api-docs/**",
             "/swagger-ui.html",
             "/swagger-ui.html/**",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+            AUTH_BASE_PATH,
+            USER_BASE_PATH + "/register"
     };
+
 }
