87027511d616f161d1ece31bb371ca78968b98b0
Kaua Santiago <santiago.kauajose12@gmail.com>
2026-09-01 17:27:32 -0300

    Fix: add @RequestBody to login route and PreAuthorize to /profile

--- src/main/java/io/github/kpsantiago/caca_oinbu/controller/AuthController.java
+++ src/main/java/io/github/kpsantiago/caca_oinbu/controller/AuthController.java
@@ -9,11 +9,9 @@ import io.github.kpsantiago.caca_oinbu.model.User;
 import io.github.kpsantiago.caca_oinbu.service.contract.IAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(ApiConfig.AUTH_BASE_PATH)
@@ -22,11 +20,12 @@ public class AuthController implements IAuthController {
     private final IAuthService service;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(LoginRequestDto request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequestDto request) {
         return ResponseEntity.ok(service.login(request));
     }
 
     @GetMapping("/profile")
+    @PreAuthorize("hasAnyAuthority('DRIVER', 'ADMIN', 'USER')")
     public ResponseEntity<UserResponseDto> profile() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
